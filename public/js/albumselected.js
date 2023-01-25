c497edc03e2c51e5d8681e4c4f457575dc770eb1
Glitch (spotify-album-poster) <none>
2023-01-25 05:10:50 +0000

    🌻👘 Checkpoint ./public/js/albumselected.js:53702932/1358

--- public/js/albumselected.js
+++ public/js/albumselected.js
@@ -1,14 +1,24 @@
 $(document).ready(function () {
-  $(document).on('click', '.albumContainer', function () {
+  $(document).on("click", ".albumContainer", function () {
     console.log("album selected: " + $(this).attr("data-value"));
-    $.post(
-      "",
-      { album: $(this).attr("data-value") },
-      function (album, status) {
-        alert(album.name + "\n" + album.total_tracks + "\n" + album.label + "\n" + album.release_date + "\n" + album.copyrights[0].text);
-        const tracks = album.tracks.items.reduce(function (arr, track) {return arr.concat(track.name)}).join("\n");
-        alert(tracks);
-      }
-    );
+    $.post("", { album: $(this).attr("data-value") }, function (album, status) {
+      alert(
+        album.name +
+          "\n" +
+          album.total_tracks +
+          "\n" +
+          album.label +
+          "\n" +
+          album.release_date +
+          "\n" +
+          album.copyrights[0].text
+      );
+      const tracks = album.tracks.items
+        .map(function (track) {
+          return track.track_number + " " + track.name.toUpperCase();
+        })
+        .join("\n");
+      alert(tracks);
+    });
   });
 });
