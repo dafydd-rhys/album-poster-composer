c05a826e68c773a93a7bf1da0b8dfe80e4bdc8a2
Glitch (spotify-album-poster) <none>
2023-06-17 05:54:49 +0000

    🏋🚙 Checkpoint ./public/poster_v1.html:53705974/1178 ./server.js:53705974/150

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ fastify.post("/", function (request, reply) {
         }
       },
       function (err) {
-        console.error(err);
+        console.log(err);
         return reply.code(404).send();
       }
     );
