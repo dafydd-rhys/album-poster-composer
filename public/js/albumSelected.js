270d8de8ed931f6c09fec4e5b99b91820824094d
Glitch (spotify-album-poster) <none>
2024-08-04 20:31:53 +0000

    ⚾️👶 Checkpoint ./public/js/albumSelected.js:53705974/770

--- public/js/albumSelected.js
+++ public/js/albumSelected.js
@@ -1,6 +1,9 @@
-import { handleAlbumClick } from './eventHandlers.js';
+import { handleAlbumClick, handleAlbumProceed } from './eventHandlers.js';
+import { handleDesignSelection } from './designSelection.js'; // Assuming you have a file for design selection
 
 $(document).ready(function () {
     $(document).on("click", ".albumContainer", handleAlbumClick);
+    $(document).on("click", ".design", handleDesignSelection); // Bind design selection
+    $('#proceed').on('click', handleAlbumProceed); // Bind proceed button
     $('#proceed').prop('disabled', true); // Ensure button is disabled initially
 });
