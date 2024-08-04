1303dc890663df6aa9d3ad3587e0860e42074567
Glitch (spotify-album-poster) <none>
2024-08-04 18:04:13 +0000

    👣🎎 Checkpoint ./.glitch-assets:53705974/425 ./src/pages/index.hbs:53705974/182 ./public/js/albumSelected.js:53705974/43 ./public/js/eventHandlers.js:53705974/798 ./public/style.css:53705974/951

--- public/js/albumSelected.js
+++ public/js/albumSelected.js
@@ -1,6 +1,5 @@
 import { handleAlbumClick } from './eventHandlers.js';
 
 $(document).ready(function () {
-    console.log("Ready"); // Add this line
     $(document).on("click", ".albumContainer", handleAlbumClick);
 });
--- public/js/eventHandlers.js
+++ public/js/eventHandlers.js
@@ -1,7 +1,15 @@
 import { fetchAlbumDetails } from './api.js';
 import { updateAlbumUI } from './albumDetails.js';
 
-export function handleAlbumClick() {
+export function handleAlbumClick(event) {
+    // Remove the border from any previously selected album
+    $(".albumContainer").removeClass("selected");
+
+    // Add the border to the clicked album
+    $(event.currentTarget).addClass("selected");
+}
+
+export function handleAlbumProceed() {
     let albumNumber = $(this).index();
     const albumContainer = $(this);
     
