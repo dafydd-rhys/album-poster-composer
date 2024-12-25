4866530f202bc40becac6a53659d406e3c457724
Glitch (spotify-album-poster) <none>
2024-12-25 01:59:48 +0000

    👩‍👩‍👦🐷 Checkpoint ./config/spotify.js:53705974/2681

--- config/spotify.js
+++ config/spotify.js
@@ -2,22 +2,26 @@ const SpotifyWebApi = require("spotify-web-api-node");
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET
+  clientSecret: process.env.CLIENT_SECRET,
 });
 
 let tokenExpirationEpoch;
 
 const initializeSpotify = () => {
   spotifyApi.clientCredentialsGrant().then(
-    function(data) {
+    function (data) {
       spotifyApi.setAccessToken(data.body["access_token"]);
       spotifyApi.setRefreshToken(data.body["refresh_token"]);
-      tokenExpirationEpoch = new Date().getTime() / 1000 + data.body["expires_in"];
+      tokenExpirationEpoch =
+        new Date().getTime() / 1000 + data.body["expires_in"];
       console.log("Got an access token: " + spotifyApi.getAccessToken());
       console.log("Got a refresh token: " + spotifyApi.getRefreshToken());
     },
-    function(err) {
-      console.log("Something went wrong when retrieving an access token!", err.message);
+    function (err) {
+      console.log(
+        "Something went wrong when retrieving an access token!",
+        err.message
+      );
     }
   );
 };
@@ -25,12 +29,13 @@ const initializeSpotify = () => {
 const refreshToken = () => {
   if (tokenExpirationEpoch < new Date().getTime() / 1000) {
     spotifyApi.refreshAccessToken().then(
-      function(data) {
-        tokenExpirationEpoch = new Date().getTime() / 1000 + data.body["expires_in"];
+      function (data) {
+        tokenExpirationEpoch =
+          new Date().getTime() / 1000 + data.body["expires_in"];
         spotifyApi.setAccessToken(data.body["access_token"]);
         console.log("Refreshed token");
       },
-      function(err) {
+      function (err) {
         console.log("Could not refresh the token.", err.message);
       }
     );
